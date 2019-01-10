4e054744dd72fff45e2d853fb36ccd8d7c7447e8
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-10 17:15:29 -0500

    test post TODO routes

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ app.post('/todos', (req,res) => {
   todo.save().then((doc)=>{
     res.send(doc);
   }, (e) => {
-    res.send(e);
+    res.status(400).send(e);
   });
 });
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
+
+module.exports = {app};
