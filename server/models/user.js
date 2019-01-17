90de29f55e12e9a9b5ad0a642646b6fc8e508a7a
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-17 16:57:09 -0500

    Added GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,6 +32,23 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try{
+    decoded = jwt.verify(token, 'abc123')
+  } catch(e) {
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
+
 UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -108,6 +109,11 @@ app.post('/users', (req, res) => {
     res.status(400).send(e)
   });
 });
+
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
