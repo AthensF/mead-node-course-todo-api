e83cecf980bf3f4938d2ed7874f10a989ab438d1
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-17 21:34:10 -0500

    added hashing for password middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,17 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
-var message = "I am  user number 3"
-var hash = SHA256(message).toString();
-console.log(`Message ${message}`);
-console.log(`Hash ${hash}`);
+var password = 'abc123!'
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$6hpKJ1/PjJ2PbeBZ4.RyauQenxcOWeSD6DDRooFwbsH99EuoCSOGG'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
