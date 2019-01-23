3de6c133f1398219d1b820d01d7c4b8bbf4c1665
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-23 17:38:25 -0500

    Added POST users/login

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,26 @@ UserSchema.statics.findByToken = function(token) {
   })
 }
 
+UserSchema.statics.findByCredentials = function(email,password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if(!user){
+      //no user case
+      return Promise.reject();
+    }
+    // user exists case
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  })
+}
+
 UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
--- server/server.js
+++ server/server.js
@@ -110,10 +110,21 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email','password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+      res.status(400).send(e)
+    })
+});
 
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
-})
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
