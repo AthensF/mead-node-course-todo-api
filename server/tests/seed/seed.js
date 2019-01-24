b3f70f27738cc59b3c6db178378a1454592bb65a
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-24 17:32:09 -0500

    added config files

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -13,7 +13,7 @@ const users = [
   password: "password!",
   tokens: [{
     access: "auth",
-    token: jwt.sign({_id: userOneId.toHexString(), access: "auth"}, 'abc123').toString()
+    token: jwt.sign({_id: userOneId.toHexString(), access: "auth"}, process.env.JWT_SECRET).toString()
   }]
   },
   {_id: userTwoId,
@@ -21,7 +21,7 @@ const users = [
   password: "passwordA",
   tokens: [{
     access: "auth",
-    token: jwt.sign({_id: userTwoId.toHexString(), access: "auth"}, 'abc123').toString()
+    token: jwt.sign({_id: userTwoId.toHexString(), access: "auth"}, process.env.JWT_SECRET).toString()
   }]
   }
 ];
