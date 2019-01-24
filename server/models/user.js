a0f697ebaed0598a93e391ba700a156627f3ae23
Athens Fitzcheung <athens@Athenss-MacBook-Pro.local>
2019-01-24 14:58:30 -0500

    add DELETE users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -89,6 +89,15 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this;
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.pre('save', function(next) {
   var user = this;
   if (user.isModified('password')){
